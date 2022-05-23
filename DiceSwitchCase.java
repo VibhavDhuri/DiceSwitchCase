2645103a9728b14cb93973b79cc2502e89e35d17
VibhavDhuri <vibhavpdhuri@gmail.com>
2022-05-23 18:21:08 +0530

    Added: Dice Switch Case project

---
+++ DiceSwitchCase.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class DiceSwitchCase {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int diceNum = 1 + random.nextInt(6);
+		System.out.println("The dice number is: " + diceNum);
+
+		switch (diceNum) {
+		case 1:
+			System.out.println("One");
+			break;
+		case 2:
+			System.out.println("Two");
+			break;
+		case 3:
+			System.out.println("Three");
+			break;
+		case 4:
+			System.out.println("Four");
+			break;
+		case 5:
+			System.out.println("Five");
+			break;
+		default:
+			System.out.println("Six");
+			break;
+		}
+	}
+
+}
